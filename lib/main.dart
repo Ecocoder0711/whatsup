4b6660aa72c1c57ee49442d3ff7e8f3da3c0704e
Ecocoder0711 <sakshamkashyapgame80@gmail.com>
2025-08-09 16:11:33 +0530

    What's_up

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Whatsup());
+}
+
+class Whatsup extends StatelessWidget {
+  const Whatsup({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
