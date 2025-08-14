2ecee7ff159aeeeb921b26fb15ba998cf93b388d
Ecocoder0711 <sakshamkashyapgame80@gmail.com>
2025-08-14 23:31:39 +0530

    splash_page_completed

--- lib/ui/screens/splash/splash_screen.dart
+++ lib/ui/screens/splash/splash_screen.dart
@@ -1,10 +1,42 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:whatsup/core/constants/string.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  // Timer? _timer;
+  @override
+  void initState() {
+    super.initState();
+    // _timer = Timer(const Duration(seconds: 3), () {
+    //   Navigator.pushNamed(context, wrapper);
+    // });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    // _timer?.cancel();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(frame, height: 1.sh, width: 1.sw, fit: BoxFit.cover),
+          Center(child: Image.asset(logosplashpage, height: 170,width: 170,),)
+        ],
+      ),
+    );
   }
 }
